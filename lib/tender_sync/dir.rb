b7437d27140a0b57a6c4f26a528fe9c5f9e3f3f3
rick <technoweenie@gmail.com>
2010-02-18 15:16:28 -0800

    add lazy reading bodies on TenderSync::Dir::File

--- lib/tender_sync/dir.rb
+++ lib/tender_sync/dir.rb
@@ -1,6 +1,14 @@
 # Special hash where keys are sections and values are arrays of article files
 class TenderSync::Dir < Hash
-  class File < Struct.new(:filename, :body)
+  class File < Struct.new(:filename, :raw_body)
+    def initialize(*args)
+      super
+      @body = nil
+    end
+
+    def body
+      @body ||= (raw_body.respond_to?(:call) ? raw_body.call : raw_body).to_s
+    end
   end
 
   def add(section, file)
